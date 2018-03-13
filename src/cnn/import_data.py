931d807853448c8023c47fefbf73edf899167648
chenying-wang <wangchenying96@gmail.com>
2018-03-13 12:39:26 +0800

    Update data import

--- src/cnn/import_data.py
+++ src/cnn/import_data.py
@@ -4,7 +4,17 @@ import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-filenames = [""]
+DATA_BASE_DIR = "..\\..\\training_dataset\\"
+DATA_NAME = "TrainingDataset"
+RESOLUTION = ["1024x576", "2560x1440", "4096X2176"]
+FRAME_RATE = 30
+FILENAME_EXTENSION = ".csv"
+SAMPLE_LENGTH = 1
+
+FEATURE_WIDTH = 64
+FEATURE_HEIGHT = FEATURE_WIDTH
+FEATURE_SIZE = FEATURE_WIDTH * FEATURE_HEIGHT
+LABEL_SIZE = 1
 
 def _parse_line(line):
 	RECORD_DEFAULT = [[0.0] for i in range(FEATURE_SIZE + LABEL_SIZE)]
@@ -13,19 +23,21 @@ def _parse_line(line):
 	label = record[FEATURE_SIZE : FEATURE_SIZE + LABEL_SIZE]
 	return features, label
 
-FEATURE_WIDTH = 16
-FEATURE_HEIGHT = 16
-FEATURE_SIZE = FEATURE_WIDTH * FEATURE_HEIGHT
-LABEL_SIZE = 1
+data_filenames = []
+for i in range(len(RESOLUTION)):
+	path = DATA_BASE_DIR
+	path += DATA_NAME + "_" + RESOLUTION[i] + "_" + str(FRAME_RATE) + "_"
+	path += str(FEATURE_WIDTH) + FILENAME_EXTENSION
+	data_filenames.append(path)
 
-dataset = tf.data.TextLineDataset(filenames).map(_parse_line)
+dataset = tf.data.TextLineDataset(data_filenames).map(_parse_line)
 iterator = dataset.make_initializable_iterator()
 next_element = iterator.get_next()  
 
 with tf.Session() as sess:
 	sess.run(iterator.initializer)
 	try:
-		while True:
+		for i in range (SAMPLE_LENGTH):
 			print(sess.run(next_element))
 	except tf.errors.OutOfRangeError:
 		print("END!")
