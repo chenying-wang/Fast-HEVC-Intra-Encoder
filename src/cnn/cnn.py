698fa893fb3242d8f1ebc64708ca87a395c146bb
chenying-wang <wangchenying96@gmail.com>
2018-03-15 22:09:09 +0800

    Add train

--- src/cnn/cnn.py
+++ src/cnn/cnn.py
@@ -1,21 +1,18 @@
-import os
 import tensorflow as tf
 import numpy as np
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.logging.set_verbosity(tf.logging.INFO)
 
-import import_data
+import dataset
 
-SAMPLE_LENGTH = 1
+MODEL_DIR = ".\\.tmp"
+BATCH_SIZE = 100
 
-
-
-def cnn_model_fn(features, lables, mode):
-
+def inference(features):
 	# Layer #0: Input 16x16x1
 	input_layer = tf.reshape(
 		tensor = features,
-		shape = [-1, import_data.FEATURE_WIDTH, import_data.FEATURE_HEIGHT, 1]
+		shape = [-1, dataset.FEATURE_WIDTH, dataset.FEATURE_HEIGHT, 1]
 	)
 	
 	# Layer #1: Conv 4x4x128
@@ -24,12 +21,15 @@ def cnn_model_fn(features, lables, mode):
 		filters = 128,
 		kernel_size = [4, 4],
 		padding = "same",
-		activation = tf.nn.relu
+		activation = tf.nn.relu,
+		kernel_initializer = tf.random_normal_initializer(mean = 0.0, stddev = 1.0),
+		name = "conv1"
 	)
 	pool1 = tf.layers.max_pooling2d(
 		inputs = conv1,
 		pool_size = [4, 4],
-		strides = 4
+		strides = 4,
+		name = "pool1"
 	)
 
 	# Layer #2: Conv 2x2x256
@@ -38,61 +38,82 @@ def cnn_model_fn(features, lables, mode):
 		filters = 256,
 		kernel_size = [2, 2],
 		padding = "same",
-		activation = tf.nn.relu
+		activation = tf.nn.relu,
+		kernel_initializer = tf.random_normal_initializer(mean = 0.0, stddev = 1.0),
+		name = "conv2"
 	)
 	pool2 = tf.layers.max_pooling2d(
 		inputs = conv2,
 		pool_size = [2, 2],
-		strides = 2
+		strides = 2,
+		name = "pool2"
 	)
 
 	# Layer #3: Concat 1024
 	pool2_flat = tf.reshape(
 		tensor = pool2,
-		shape = [-1, 1024]  #[-1, tf.cast(tf.divide(tf.size(pool2), tf.shape(pool2)[0]), tf.int32)]
+		shape = [-1, 1024],  #[-1, tf.cast(tf.divide(tf.size(pool2), tf.shape(pool2)[0]), tf.int32)]
+		name = "pool2_flat"
 	)
 	
 	# Layer #4: Dense 256
 	dense1 = tf.layers.dense(
 		inputs = pool2_flat,
 		units = 256,
-		activation = tf.nn.relu
+		activation = tf.nn.relu,
+		name = "dense1"
 	)
 
 	# Layer #5: Dense 32
 	dense2 = tf.layers.dense(
 		inputs = dense1,
 		units = 32,
-		activation = tf.nn.relu
+		activation = tf.nn.relu,
+		name = "dense2"
 	)
 
 	# Layers #6: Logits 2
 	logits = tf.layers.dense(
 		inputs = dense2,
 		units = 2,
-		activation = tf.nn.relu
+		activation = tf.nn.relu,
+		name = "logits"
 	)
 
 	# Layers #7: Softmax 2
 	softmax = tf.nn.softmax(
+		logits = logits,
+		name = "softmax_tensor"
+	)
+	return logits, softmax
+
+def train(labels, logits):
+	
+	loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+		labels = labels,
 		logits = logits
 	)
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.1)
+	optimizer.minimize(loss)
+	return labels, logits, loss
 
-	predictions = softmax
-	return predictions
+def main(unused_argv):
+	iterator = dataset.get().make_initializable_iterator()
+	features, labels = iterator.get_next()
+	onehot_labels = tf.one_hot(indices = labels, depth = 2)
 
-dataset = import_data.get_dataset()
-iterator = dataset.make_initializable_iterator()
-next_element = iterator.get_next()
-pred = cnn_model_fn(next_element["features"], next_element["labels"], tf.estimator.ModeKeys.PREDICT)
+	logits, softmax = inference(features)
 
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-	sess.run(iterator.initializer)
-	try:
-		for i in range (SAMPLE_LENGTH):
-			print(sess.run(
-				pred
-			))
-	except tf.errors.OutOfRangeError:
-		print("END!")
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		sess.run(iterator.initializer)
+		try:
+			for i in range (1000):
+				print(sess.run(
+					train(onehot_labels, logits)
+				))
+		except tf.errors.OutOfRangeError:
+			print("END!")
+
+if __name__ == "__main__":
+	tf.app.run()
--- src/cnn/dataset.py
+++ src/cnn/dataset.py
@@ -2,29 +2,36 @@ import tensorflow as tf
 
 DATA_BASE_DIR = "..\\..\\training_dataset\\"
 DATA_NAME = "TrainingDataset"
-# RESOLUTION = ["1024x576", "2560x1440", "4096X2176"]
-RESOLUTION = ["1024x576"]
-FRAME_RATE = 30
 FILENAME_SUFFIX = ".csv.gz"
+
+FRAME_WIDTH = [1024, 2560, 4096]
+FRAME_HEIGHT = [576, 1440, 2176]
+FRAME_WIDTH = [1024]
+FRAME_HEIGHT = [576]
+FRAME_RATE = 30
+FRAME_LENGTH = 1000
 
 FEATURE_WIDTH = 16
 FEATURE_HEIGHT = FEATURE_WIDTH
 FEATURE_SIZE = FEATURE_WIDTH * FEATURE_HEIGHT
 LABEL_SIZE = 1
 
+SHUFFLE_BUFFER = 10
+
 RECORD_DEFAULT = [[0.0] for i in range(FEATURE_SIZE + LABEL_SIZE)]
 def _parse_line(line):
 	record = tf.convert_to_tensor(tf.decode_csv(line, RECORD_DEFAULT))
 	features = tf.reshape(record[0 : FEATURE_SIZE], [FEATURE_WIDTH, FEATURE_HEIGHT])
-	labels = record[FEATURE_SIZE : FEATURE_SIZE + LABEL_SIZE]
-	return {"features": features, "labels": labels}
+	labels = tf.cast(record[FEATURE_SIZE : FEATURE_SIZE + LABEL_SIZE], tf.int32)
+	return features, labels
 
-def get_dataset():
+def get():
 	data_filenames = []
-	for i in range(len(RESOLUTION)):
+	for i in range(len(FRAME_WIDTH)):
 		path = DATA_BASE_DIR
-		path += DATA_NAME + "_" + RESOLUTION[i] + "_" + str(FRAME_RATE) + "_"
+		path += DATA_NAME + "_" + str(FRAME_WIDTH[i]) + "x" + str(FRAME_HEIGHT[i]) + "_" 
+		path += str(FRAME_RATE) + "_"
 		path += str(FEATURE_WIDTH) + FILENAME_SUFFIX
 		data_filenames.append(path)
 	dataset = tf.data.TextLineDataset(data_filenames, compression_type="GZIP")
-	return dataset.map(_parse_line)
+	return dataset.map(_parse_line)#.shuffle(SHUFFLE_BUFFER)
